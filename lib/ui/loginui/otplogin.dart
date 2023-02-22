163175c91e390420d6d3f22c7a2e1ff71f837a0e
sanjucoder89 <sanjayitdeveloper@gmail.com>
2023-02-22 14:55:05 +0530

    fixed hbncsssss

--- lib/ui/loginui/otplogin.dart
+++ lib/ui/loginui/otplogin.dart
@@ -145,209 +145,211 @@ class _OtpLoginPage extends State<OtpLoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: Container(
-        color: ColorConstants.white,
-        child: Column(
-          children:<Widget>[
-            Expanded(child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  '${Strings.verify_enter_mobile_no}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.red),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Row(
+      body: SingleChildScrollView(
+        child: Container(
+          color: ColorConstants.white,
+          child: Column(
+            children:<Widget>[
+              Expanded(child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('${widget.Mobileno.substring(0, 2) + "******" + widget.Mobileno.substring(8, 10)}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: ColorConstants.AppColorPrimary),
-                    textAlign: TextAlign.center,
-                  ),
-                  Text(' पर प्राप्त OTP दर्ज करें |',
-                    style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        fontSize: 16,
-                        color: ColorConstants.black),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              ),
-              Container(
-                child: Form(
-                  key: formKey,
-                  child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 30),
-                      child: PinCodeTextField(
-                        appContext: context,
-                        pastedTextStyle: TextStyle(
-                          color: Colors.green.shade600,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      '${Strings.verify_enter_mobile_no}',
+                      style: TextStyle(
                           fontWeight: FontWeight.bold,
-                        ),
-                        length: 4,
-                        obscureText: true,
-                        obscuringCharacter: '*',
-                        blinkWhenObscuring: true,
-                        animationType: AnimationType.fade,
-                        validator: (v) {
-                          if (v!.length < 3) {
-                            return "${Strings.enter_otp_error}";
-                          } else {
-                            return null;
-                          }
-                        },
-                        pinTheme: PinTheme(
-                          inactiveFillColor: ColorConstants.map_green_color,
-                          errorBorderColor: ColorConstants.map_green_color,
-                          shape: PinCodeFieldShape.box,
-                          borderRadius: BorderRadius.circular(5),
-                          fieldHeight: 50,
-                          fieldWidth: 40,
-                          activeFillColor: Colors.white,
-                        ),
-                        cursorColor: Colors.black,
-                        animationDuration: Duration(milliseconds: 300),
-                        enableActiveFill: true,
-                        errorAnimationController: errorController,
-                        controller: textEditingController,
-                        keyboardType: TextInputType.number,
-                        boxShadows: [
-                          BoxShadow(
-                            offset: Offset(0, 1),
-                            color: Colors.black12,
-                            blurRadius: 10,
-                          )
-                        ],
-                        onCompleted: (v) {
-                          //print("Completed$v");
-                          otpvalue = v;
-                        },
-                        // onTap: () {
-                        //   print("Pressed");
-                        // },
-                        onChanged: (value) {
-                          print(value);
-                          setState(() {
-                            currentText = value;
-                            print(currentText);
-                          });
-                        },
-
-                        beforeTextPaste: (text) {
-                          //print("Allowing to paste $text");
-                          //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
-                          //but you can show anything you want here, like your pop up saying wrong paste format or etc
-                          return true;
-                        },
-                      )),
-                ),
-                margin: EdgeInsets.only(left: 60,right: 60,top: 20),
-              ),
-              GestureDetector(
-                onTap:() async {
-                  formKey.currentState!.validate();
-                  // conditions for validating
-                  if (currentText.length != 4) {
-                    errorController!.add(ErrorAnimationType
-                        .shake); // Triggering error shake animation
-                    setState(() => hasError = true);
-                  } else {
-                    var result = await Connectivity().checkConnectivity();
-                    if(result == ConnectivityResult.mobile) {
-                      setState(() {
-                        hasError = false;
-                        getCheckOtp(otpvalue);
-                        //  snackBar("OTP Verified!!");
-                      },
-                      );
-                    }else if(result == ConnectivityResult.wifi) {
-                      setState(() {
-                        hasError = false;
-                        getCheckOtp(otpvalue);
-                        //  snackBar("OTP Verified!!");
-                      },
-                      );
-                    }else if(result == ConnectivityResult.none){
-                      print("No internet connection");
-                      showInternetDialoge();
-                    }
-
-                  }
-                },
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  elevation: 6,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color:ColorConstants.AppColorPrimary,
+                          fontSize: 16,
+                          color: Colors.red),
+                      textAlign: TextAlign.center,
                     ),
-                    width: 100,
-                    height: 40,
-                    child: Center(child: Text('${Strings.next_}',style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.normal),)),
                   ),
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) =>SaMobileRegister( Message: message,Val_Flag: widget.Val_Flag,),
-                    ),
-                  );
-                },
-                child: Container(
-                  margin: EdgeInsets.only(left: 10,right: 10),
-                  height: 40,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.red, //                   <--- border color
-                      width: 3.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.white,
-
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: Text('पंजीकृत मोबाइल नंबर ',style: TextStyle(color: Colors.red,fontSize: 12),),
-                      ),Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: Text('${widget.Mobileno.substring(0, 2) + "******" + widget.Mobileno.substring(8, 10)}',style: TextStyle(color:ColorConstants.text_green,fontSize: 12),),
-                      ),Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: Text(' को बदलने के लिए क्लिक करें |',style: TextStyle(color:Colors.red,fontSize: 12),),
-                      )
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('${widget.Mobileno.substring(0, 2) + "******" + widget.Mobileno.substring(8, 10)}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: ColorConstants.AppColorPrimary),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(' पर प्राप्त OTP दर्ज करें |',
+                        style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 16,
+                            color: ColorConstants.black),
+                        textAlign: TextAlign.center,
+                      ),
                     ],
                   ),
+                  Container(
+                    child: Form(
+                      key: formKey,
+                      child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 30),
+                          child: PinCodeTextField(
+                            appContext: context,
+                            pastedTextStyle: TextStyle(
+                              color: Colors.green.shade600,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            length: 4,
+                            obscureText: true,
+                            obscuringCharacter: '*',
+                            blinkWhenObscuring: true,
+                            animationType: AnimationType.fade,
+                            validator: (v) {
+                              if (v!.length < 3) {
+                                return "${Strings.enter_otp_error}";
+                              } else {
+                                return null;
+                              }
+                            },
+                            pinTheme: PinTheme(
+                              inactiveFillColor: ColorConstants.map_green_color,
+                              errorBorderColor: ColorConstants.map_green_color,
+                              shape: PinCodeFieldShape.box,
+                              borderRadius: BorderRadius.circular(5),
+                              fieldHeight: 50,
+                              fieldWidth: 40,
+                              activeFillColor: Colors.white,
+                            ),
+                            cursorColor: Colors.black,
+                            animationDuration: Duration(milliseconds: 300),
+                            enableActiveFill: true,
+                            errorAnimationController: errorController,
+                            controller: textEditingController,
+                            keyboardType: TextInputType.number,
+                            boxShadows: [
+                              BoxShadow(
+                                offset: Offset(0, 1),
+                                color: Colors.black12,
+                                blurRadius: 10,
+                              )
+                            ],
+                            onCompleted: (v) {
+                              //print("Completed$v");
+                              otpvalue = v;
+                            },
+                            // onTap: () {
+                            //   print("Pressed");
+                            // },
+                            onChanged: (value) {
+                              print(value);
+                              setState(() {
+                                currentText = value;
+                                print(currentText);
+                              });
+                            },
 
-                ),
-              )
-            ],)),
-            Image.asset(
-              "Images/footerpcts.jpg",
-              fit: BoxFit.fitWidth,
-              alignment: Alignment.bottomLeft,
-            ),
-          ],
+                            beforeTextPaste: (text) {
+                              //print("Allowing to paste $text");
+                              //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                              //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                              return true;
+                            },
+                          )),
+                    ),
+                    margin: EdgeInsets.only(left: 60,right: 60,top: 20),
+                  ),
+                  GestureDetector(
+                    onTap:() async {
+                      formKey.currentState!.validate();
+                      // conditions for validating
+                      if (currentText.length != 4) {
+                        errorController!.add(ErrorAnimationType
+                            .shake); // Triggering error shake animation
+                        setState(() => hasError = true);
+                      } else {
+                        var result = await Connectivity().checkConnectivity();
+                        if(result == ConnectivityResult.mobile) {
+                          setState(() {
+                            hasError = false;
+                            getCheckOtp(otpvalue);
+                            //  snackBar("OTP Verified!!");
+                          },
+                          );
+                        }else if(result == ConnectivityResult.wifi) {
+                          setState(() {
+                            hasError = false;
+                            getCheckOtp(otpvalue);
+                            //  snackBar("OTP Verified!!");
+                          },
+                          );
+                        }else if(result == ConnectivityResult.none){
+                          print("No internet connection");
+                          showInternetDialoge();
+                        }
+
+                      }
+                    },
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      elevation: 6,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color:ColorConstants.AppColorPrimary,
+                        ),
+                        width: 100,
+                        height: 40,
+                        child: Center(child: Text('${Strings.next_}',style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.normal),)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) =>SaMobileRegister( Message: message,Val_Flag: widget.Val_Flag,),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(left: 10,right: 10),
+                      height: 40,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.red, //                   <--- border color
+                          width: 3.0,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.white,
+
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Text('पंजीकृत मोबाइल नंबर ',style: TextStyle(color: Colors.red,fontSize: 12),),
+                          ),Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Text('${widget.Mobileno.substring(0, 2) + "******" + widget.Mobileno.substring(8, 10)}',style: TextStyle(color:ColorConstants.text_green,fontSize: 12),),
+                          ),Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Text(' को बदलने के लिए क्लिक करें |',style: TextStyle(color:Colors.red,fontSize: 12),),
+                          )
+                        ],
+                      ),
+
+                    ),
+                  )
+                ],)),
+              Image.asset(
+                "Images/footerpcts.jpg",
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.bottomLeft,
+              ),
+            ],
+          ),
         ),
       ),
     );
